4414b97fbf9206b266602c047382b6a53f622d4c
Jef Willems <jwillems04@gmail.com>
2018-07-11 09:37:08 +0200

    Modified existing filter, to filter on name by chars.

--- src/app/shared/userFilter.pipe.ts
+++ src/app/shared/userFilter.pipe.ts
@@ -1,15 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { AppUser } from '../../models/AppUser';
 
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
-  transform(items: any[], searchText: string): any[] {
-    if (!items) {return []; }
-    if (!searchText) {return items; }
-    return items.filter( item => {
-      return Object.values(item).indexOf(searchText) > -1;
-    });
+  // transform(items: any[], searchText: string): any[] {
+  //   if (!items) {return []; }
+  //   if (!searchText) {return items; }
+  //   return items.filter( item => {
+  //     return Object.values(item).indexOf(searchText) > -1;
+  //   });
+  // }
+
+  transform(value: AppUser[], filterBy: string): AppUser[] {
+    filterBy = filterBy ? filterBy.toLocaleLowerCase() : null;
+    return filterBy ? value.filter((user: AppUser) =>
+      user.name.toLocaleLowerCase().indexOf(filterBy)
+      !== -1) : value;
   }
 
 }

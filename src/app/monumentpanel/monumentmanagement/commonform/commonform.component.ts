596482c73593f1332efa3dbad7e0d9d7b82b67d0
Jef Willems <jwillems04@gmail.com>
2018-07-19 15:33:53 +0200

    attempt to let input field from child send its changed values.

--- src/app/monumentpanel/monumentmanagement/commonform/commonform.component.ts
+++ src/app/monumentpanel/monumentmanagement/commonform/commonform.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {Information, Monument} from '../../../../models/AppUser';
 import {MonumentsService} from '../../../../services/monuments.service';
 import {ActivatedRoute} from '@angular/router';
@@ -9,13 +9,18 @@ import {MonumentmanagementComponent} from '../monumentmanagement.component';
   templateUrl: './commonform.component.html',
   styleUrls: ['./commonform.component.css']
 })
-export class CommonformComponent implements OnInit {
+export class CommonformComponent implements OnInit, AfterViewInit {
 
   @Output() returnCommonData = new EventEmitter<Monument>();
   monId: string;
   currentMonument: Monument;
+  returnMonument: Monument;
   monumentFound = false;
   areas: String[] = [];
+
+  lat: number;
+  long: number;
+
 
   constructor(private _route: ActivatedRoute, private monumentService: MonumentsService) {
 
@@ -25,6 +30,14 @@ export class CommonformComponent implements OnInit {
     this.fetchIdFromUrl();
     this.getCurrentMonument(this.monId);
     this.getAllAreas();
+  }
+
+  ngAfterViewInit() {
+    this.returnMonument = this.currentMonument;
+    const latStr = (<HTMLInputElement>document.getElementById('latitude')).value;
+    const longStr = (<HTMLInputElement>document.getElementById('longitude')).value;
+    this.returnMonument.latitude = Number(latStr);
+    this.returnMonument.longitude = Number(longStr);
 
   }
 
@@ -44,8 +57,11 @@ export class CommonformComponent implements OnInit {
   getCurrentMonument(id: string) {
     this.monumentService.getMonumentById(id).subscribe(res => {
       this.currentMonument = res;
-      this.returnCommonData.emit(this.currentMonument);
       console.log('Response is: ' + this.currentMonument.information[0].name);
+
+
+      this.returnCommonData.emit(this.currentMonument);
+
     });
   }
 
